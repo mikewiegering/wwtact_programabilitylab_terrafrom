fb0c54d4e8c0045da29135de65e4db0eadfdb288
mikewiegering <mike.wiegering@gmail.com>
2020-06-01 16:02:34 +0000

    added variales file and fixed .tfvars file

--- provider.tf
+++ provider.tf
@@ -4,4 +4,4 @@ provider "vsphere" {
   vsphere_server = var.vsphere_server
 
   # If you have a self-signed cert
-  allow_unverified_ssl = true
+  allow_unverified_ssl = True
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "vsphere_user" {
+  default = "username"
+}
+
+variable "vsphere_password" {
+  default = "password"
+}
+
+variable "vsphere_server" {
+  default = "server hostname"
+}
