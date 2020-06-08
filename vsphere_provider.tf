65e326a72a6a4c59e9d3e078a351a16fb053725f
mikewiegering <mike.wiegering@gmail.com>
2020-06-08 13:46:39 -0700

    addind additions to vsphere provider module

--- vsphere_provider.tf
+++ vsphere_provider.tf
@@ -7,3 +7,31 @@ provider "vsphere" {
   allow_unverified_ssl = true
 }
 
+############################################################################
+####### Defining vcenter data and querying vcenter for object ids   #######
+############################################################################
+
+data "vsphere_datacenter" "dc" {
+  name = "Datacenter"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "vStorage1"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = "Lab"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+#data "vsphere_resource_pool" "pool" {
+#  name          = "LabPool"
+#  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+#}
+
+data "vsphere_network" "network" {
+  name          = "Sim-Mgmt"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
