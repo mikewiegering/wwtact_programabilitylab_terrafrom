5f7e99ddcc9f3403dade94e8408384ed06bbf4fe
mikewiegering <mike.wiegering@gmail.com>
2020-06-09 20:24:36 +0000

    added provisioning of dvs to single host

--- vsphere_network.tf
+++ vsphere_network.tf
@@ -23,18 +23,18 @@ variable "network_interfaces" {
 #  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 #}
 
-#resource "vsphere_distributed_virtual_switch" "dvs" {
- # name          = "terraform-test-dvs"
- # datacenter_id = "${data.vsphere_datacenter.dc.id}"
+resource "vsphere_distributed_virtual_switch" "dvs" {
+  name          = "terraform-test-dvs"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 
- # uplinks         = ["uplink1", "uplink2", "uplink3"]
- # active_uplinks  = ["uplink1", "uplink2"]
- # standby_uplinks = ["uplink2", "uplink3"]
+  uplinks         = ["uplink1", "uplink2", "uplink3"]
+  active_uplinks  = ["uplink1", "uplink2"]
+  standby_uplinks = ["uplink2", "uplink3"]
 
- # host {
-  #  host_system_id = "${data.vsphere_host.esxi1.id}"
-   # devices        = ["${var.network_interfaces}"]
-  #}
+  host {
+    host_system_id = "${data.vsphere_host.host.id}"
+    devices        = ["${var.network_interfaces}"]
+  }
 
   #host {
    # host_system_id = "${data.vsphere_host.esxi2.id}"
