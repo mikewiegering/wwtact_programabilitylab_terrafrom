325ecb78991c34819b43c178e807cb57cbf08d95
mikewiegering <mike.wiegering@gmail.com>
2020-06-09 22:34:19 +0000

    added get hosts

--- vsphere_network.tf
+++ vsphere_network.tf
@@ -3,11 +3,24 @@
  # name = "Datacenter"
 #}
 
-data "vsphere_host" "host" {
- count         = "${length(var.esxi_hosts)}"
- name          = "${var.esxi_hosts[count.index]}"
- datacenter_id = "${data.vsphere_datacenter.dc.id}"
+# data "vsphere_host" "host" {
+#  count         = "${length(var.esxi_hosts)}"
+#  name          = "${var.esxi_hosts[count.index]}"
+#  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+# }
+
+data "vsphere_host" "host1" {
+  name          = "esxi1.lab.local"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
+
+data "vsphere_host" "host2" {
+  name          = "esxi2.lab.local"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+
+
 
 resource "vsphere_distributed_virtual_switch" "dvs" {
   name          = "tf_test_dvs"
@@ -17,15 +30,15 @@ resource "vsphere_distributed_virtual_switch" "dvs" {
   active_uplinks  = ["uplink1", "uplink2"]
   standby_uplinks = ["uplink3", "uplink4"]
 
-    host {
-    host_system_id = "${data.vsphere_host.host.0.id}"
-    devices        = ["vmnic3", "vmnic4", "vmnic5"]
-  }
+#     host {
+#     host_system_id = "${data.vsphere_host.host.0.id}"
+#     devices        = ["vmnic3", "vmnic4", "vmnic5"]
+#   }
 
-  host {
-    host_system_id = "${data.vsphere_host.host.1.id}"
-    devices        = ["vmnic3", "vmnic4", "vmnic5"]
-  }
+#   host {
+#     host_system_id = "${data.vsphere_host.host.1.id}"
+#     devices        = ["vmnic3", "vmnic4", "vmnic5"]
+#   }
   
  
     
