577f9dabc089eab01ee02eac2d258964c3104a88
mikewiegering <mike.wiegering@gmail.com>
2020-06-09 18:45:23 +0000

    updated host data pull in vs provider module

--- vsphere_provider.tf
+++ vsphere_provider.tf
@@ -35,12 +35,12 @@ data "vsphere_network" "network" {
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
-data "vsphere_host" "esxi1" {
+data "vsphere_host" "host" {
   name          = "esxi1"
-  datacenter_id = "${data.vsphere_datacenter.datacenter.id}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
-data "vsphere_host" "esxi2" {
+data "vsphere_host" "host" {
   name          = "esxi2"
-  datacenter_id = "${data.vsphere_datacenter.datacenter.id}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
