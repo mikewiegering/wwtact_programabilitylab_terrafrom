9342925a6122ec42f542de6802f29f01af7a6f81
mikewiegering <mike.wiegering@gmail.com>
2020-06-18 13:10:08 -0700

    attempting to bring the csv file and use it on a new bd

---
+++ aci_network.tf
@@ -0,0 +1,9 @@
+
+csvdata = csvdecode(network_csv)
+
+data "aci_bridge_domain" "bd" {
+  foreach = { for bd in local.csvdata : bd.aci_bd => bd }
+  
+  tenant_dn  = "${aci_tenant.tftenant.id}"
+  name       = each.value.aci_bd
+}
