cdc71c2bfc6c7577b79de1a371ab253fd8c3ccab
mikewiegering <mike.wiegering@gmail.com>
2020-06-18 15:16:07 -0700

    still trying to get the csv file to work

--- aci_network.tf
+++ aci_network.tf
@@ -7,5 +7,5 @@ data "aci_bridge_domain" "bd" {
   count = "${length(slice(split("\n", lookup(data.local_file.network_csv.outputs, "filedata")), 1, length(split("\n", lookup(data.local_file.network_csv.outputs, "filedata")))))}"
   
   tenant_dn  = "${aci_tenant.tftenant.id}"
-  name       = local.filedata[count.index].aci_bd
+  name       = local.network_csv[count.index].aci_bd
 }
