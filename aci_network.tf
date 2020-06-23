feab164fbaa70ad1dd24b51fc94ef2e4bd15dd46
mikewiegering <mike.wiegering@gmail.com>
2020-06-23 15:38:07 -0700

    added app profile and vrf to aci_network

--- aci_network.tf
+++ aci_network.tf
@@ -4,6 +4,22 @@ csvdata = csvdecode(file("./network_csv.csv"))
 
 }
 
+
+
+resource "aci_application_profile" "tfapp" {
+  count      = length(local.csvdata)
+  tenant_dn  = "uni/tn-tftenant/ap-${local.csvdata[count.index].aci_app_profile}"
+  name       = local.csvdata[count.index].aci_app_profile
+
+}
+
+resource "aci_vrf" "tfvrf" {
+  count                  = length(local.csvdata)
+  tenant_dn              = "uni/tn-tftenant/ctx-${local.csvdata[count.indext].aci_vrf}"
+  name                   = local.csvdata[count.index].aci_vrf
+  
+}
+
 resource "aci_bridge_domain" "tfbd" {
         count                       = length(local.csvdata)
         tenant_dn                   = "${aci_tenant.tftenant.id}"
@@ -15,7 +31,6 @@ resource "aci_bridge_domain" "tfbd" {
 
 resource "aci_subnet" "tfsubnet" {
         count            = length(local.csvdata)
-        # bridge_domain_dn = local.csvdata[count.index].bd_dn
         bridge_domain_dn = "uni/tn-tftenant/BD-${local.csvdata[count.index].aci_bd}"
         description      = local.csvdata[count.index].aci_subnet
         ip               = local.csvdata[count.index].aci_subnet
