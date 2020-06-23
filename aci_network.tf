bf0386f4d91ccce95702276287db92f22ca96d15
mikewiegering <mike.wiegering@gmail.com>
2020-06-23 11:49:35 -0700

    attempting new  bd using csv

--- aci_network.tf
+++ aci_network.tf
@@ -5,8 +5,10 @@ csvdata = csvdecode(file("./network_csv.csv"))
 }
 
 resource "aci_bridge_domain" "tfbd" {
+        count                       = length(local.csvdata)
         tenant_dn                   = "${aci_tenant.tftenant.id}"
         description                 = local.csvdata[count.index].aci_bd
         name                        = local.csvdata[count.index].aci_bd
+
         
     }
