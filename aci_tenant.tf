1c681b88f86decbe2b95f4938933dc9a12072f83
mikewiegering <mike.wiegering@gmail.com>
2020-07-07 14:18:28 -0700

    fixed aci_tenant dn names

--- aci_tenant.tf
+++ aci_tenant.tf
@@ -3,14 +3,14 @@ resource "aci_tenant" "tftenant" {
 }
 
 resource "aci_vrf" "tfvrf" {
-  tenant_dn              = "uni/tn-tftenant/ctx-test_vrf_1}"
+  tenant_dn              = "uni/tn-tftenant/ctx-test_vrf_1"
   name                   = "test_vrf_1"
   
 }
 
 
 resource "aci_application_profile" "tfapp" {
-  tenant_dn  = "uni/tn-tftenant/ap-access}"
+  tenant_dn  = "uni/tn-tftenant/ap-access"
   name       = "access"
 
 }
