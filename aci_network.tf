183a90cb0be102cf7113740649a41f7b0eb8ef4d
mikewiegering <mike.wiegering@gmail.com>
2020-11-10 15:55:11 -0800

    still trying to fix aci_network

--- aci_network.tf
+++ aci_network.tf
@@ -4,22 +4,25 @@ csvdata = csvdecode(file("./network_csv.csv"))
 
 }
 
-resource "aci_bridge_domain" "tfbd" {
-        for_each = { for bd in local.csvdata : bd.aci_subnet => bd }
-        tenant_dn                   = "${aci_tenant.tftenant.id}"
-        description                 = each.value.aci_bd
-        name                        = each.value.aci_bd
+# resource "aci_bridge_domain" "tfbd" {
+#         for_each = { for bd in local.csvdata : bd.aci_subnet => bd }
+#         tenant_dn                   = "${aci_tenant.tftenant.id}"
+#         description                 = each.value.aci_bd
+#         name                        = each.value.aci_bd
 
         
-    }
+#     }
 
-resource "aci_subnet" "tfsubnet" {
-        for_each = { for subnet in local.csvdata : subnet.aci_subnet => subnet }
+# resource "aci_subnet" "tfsubnet" {
+#         for_each = { for subnet in local.csvdata : subnet.aci_subnet => subnet }
         
-        parent_dn        = "${aci_bridge_domain.bdname.id}"
-        description      = each.value.aci_subnet
-        ip               = each.value.aci_subnet
+#         parent_dn        = "uni/tn-tftenant/BD-${csvdata.aci_bd}"
+#         description      = csvdata.aci_subnet
+#         ip               = csvdata.aci_subnet
        
-    } 
+#     } 
 
-    
+
+output "csvtest" {
+   csvdata.aci_bd[*]
+}
