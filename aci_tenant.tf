7af613af6d635125ebcf508acd6f792a6d78f189
mikewiegering <mike.wiegering@gmail.com>
2020-11-10 16:34:37 -0800

    still trying to fix aci_network

--- aci_tenant.tf
+++ aci_tenant.tf
@@ -2,18 +2,15 @@
 
 resource "aci_tenant" "tftenant" {
   name        = "tftenant"
-  annotation  = "tag tenant"
 }
 
 resource "aci_vrf" "tfvrf" {
   tenant_dn              = "uni/tn-tftenant"
   name                   = "test_vrf_1"
-  
 }
 
 
 resource "aci_application_profile" "tfapp" {
   tenant_dn  = "uni/tn-tftenant"
   name       = "access"
-
 }
