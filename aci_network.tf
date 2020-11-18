b2847a2241ba2f993e139d7ddc94ab1fe88bc200
mikewiegering <mike.wiegering@gmail.com>
2020-11-18 19:10:09 +0000

    modified the aci_network file to include description field

--- aci_network.tf
+++ aci_network.tf
@@ -7,7 +7,7 @@ csvdata = csvdecode(file("./network_csv.csv"))
 resource "aci_bridge_domain" "tfbd" {
         for_each = { for bd in local.csvdata : bd.aci_subnet => bd }
         tenant_dn                   = "${aci_tenant.tftenant.id}"
-        description                 = each.value.aci_bd
+        description                 = each.value.bd_description
         name                        = each.value.aci_bd
 
         
@@ -16,7 +16,7 @@ resource "aci_bridge_domain" "tfbd" {
 resource "aci_subnet" "tfsubnet" {
         for_each = { for subnet in local.csvdata : subnet.aci_subnet => subnet }
         bridge_domain_dn        = "uni/tn-tftenant/BD-${each.value.aci_bd}"
-        description      = each.value.aci_subnet
+        description      = each.value.subnet_description
         ip               = each.value.aci_subnet
        
     } 
